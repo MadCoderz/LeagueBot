c4732560ea8b2f36f40f4725e28ac20a14d3e4e8
MadCoderz <mail223@linuxmail.org>
2014-06-16 13:09:25 +0300

    fix lots of derps

--- activator_beta.lua
+++ activator_beta.lua
@@ -26,16 +26,27 @@ local timer = os.clock()
 local bluePill = nil
 
 ADItems, ad = uiconfig.add_menu('AD Aggressive Items')
-ad.keytoggle('botRK', 'Blade of the Ruined King', true)
-ad.keytoggle('Hydra', 'Ravenous Hydra', true)
-ad.keytoggle('Tiamat', 'Tiamat', true)
+ad.checkbutton('botRK', 'Blade of the Ruined King', true)
+ad.checkbutton('Hydra', 'Ravenous Hydra', true)
+ad.checkbutton('Tiamat', 'Tiamat', true)
 
 APItems, ap = uiconfig.add_menu('AP Aggressive Items')
-ap.keytoggle('DFG', 'Deathfire Grasp', true)
+ap.checkbutton('DFG', 'Deathfire Grasp', true)
+
+local SummonerSpells =
+	{
+		Ignite = {Key = nil, Name = "SummonerDot"},
+		Exhaust = {Key = nil, Name = "SummonerExhaust"},
+		Heal = {Key = nil, Name = "SummonerHeal"},
+		Clarity = {Key = nil, Name = "SummonerMana"},
+		Barrier = {Key = nil, Name = "SummonerBarrier"},
+		Clairvoyance = {Key = nil, Name = "SummonerClairvoyance"},
+		Cleanse = {Key = nil, Name = "SummonerBoost"}
+	}
 
 Spells, sspells = uiconfig.add_menu('Summoner Spells')
 if (myHero.SummonerD or myHero.SummonerF) == SummonerSpells.Ignite.Name then
-	sspells.keytoggle('ign', 'Auto Ignite', true)
+	sspells.checkbutton('ign', 'Auto Ignite', true)
 	IgniteOptions, ignopt = uiconfig.add_menu('Ignite Options')
 	ignopt.checkbutton('BurnGA', 'Ignite Guardian Angel', true)
 	ignopt.checkbutton('BurnEgg', 'Ignite Anivia even Eggnivia passive', false)
@@ -44,19 +55,18 @@ if (myHero.SummonerD or myHero.SummonerF) == SummonerSpells.Ignite.Name then
 	if myHero.SummonerD == SummonerSpells.Ignite.Name then ignKey = 'D'
 	else ignKey = 'F'
 	end
-end
 elseif (myHero.SummonerD or myHero.SummonerF) == SummonerSpells.Exhaust.Name then
-	sspells.keytoggle('exh', 'Auto Exhaust', true)
+	sspells.checkbutton('exh', 'Auto Exhaust', true)
 elseif (myHero.SummonerD or myHero.SummonerF) == SummonerSpells.Heal.Name then
-	sspells.keytoggle('heal', 'Auto Heal', true)
+	sspells.checkbutton('heal', 'Auto Heal', true)
 elseif (myHero.SummonerD or myHero.SummonerF) == SummonerSpells.Clarity.Name then
-	sspells.keytoggle('cla', 'Auto Clarity', true)
+	sspells.checkbutton('cla', 'Auto Clarity', true)
 elseif (myHero.SummonerD or myHero.SummonerF) == SummonerSpells.Barrier.Name then
-	sspells.keytoggle('brr', 'Auto Barrier', true)
+	sspells.checkbutton('brr', 'Auto Barrier', true)
 elseif (myHero.SummonerD or myHero.SummonerF) == SummonerSpells.Clairvoyance.Name then
-	sspells.keytoggle('clv', 'Auto Clairvoyance', true)
+	sspells.checkbutton('clv', 'Auto Clairvoyance', true)
 	elseif (myHero.SummonerD or myHero.SummonerF) == SummonerSpells.Cleanse.Name then
-	sspells.keytoggle('cls', 'Auto Cleanse', true)
+	sspells.checkbutton('cls', 'Auto Cleanse', true)
 else
 	print('Script is broken or outdated.')
 end
@@ -73,16 +83,6 @@ pots.slider('Crystalline_Flask_Value', 'Crystalline Flask Value', 0, 100, 75, ni
 pots.slider('Elixir_of_Fortitude_Value', 'Elixir of Fortitude Value', 0, 100, 30, nil, true)
 pots.slider('Biscuit_Value', 'Biscuit Value', 0, 100, 60, nil, true)
 pots.permashow('AutoPotions')
-local SummonerSpells =
-	{
-		Ignite = {Key = nil, Name = "SummonerDot"},
-		Exhaust = {Key = nil, Name = "SummonerExhaust"},
-		Heal = {Key = nil, Name = "SummonerHeal"},
-		Clarity = {Key = nil, Name = "SummonerMana"},
-		Barrier = {Key = nil, Name = "SummonerBarrier"},
-		Clairvoyance = {Key = nil, Name = "SummonerClairvoyance"},
-		Cleanse = {Key = nil, Name = "SummonerBoost"}
-	}
 
 function eventLoop()
 	if myHero.dead == 0 then
@@ -90,8 +90,8 @@ function eventLoop()
 		if ad.Tiamat then Tiamat() end
 		if ad.Hydra then Hydra() end
 		if ap.DFG then DFG() end
-		if Spells.ign then ign() end
-		if Potions.AutoPotions then potions() end
+		if sspells.ign then ign() end
+		if pots.AutoPotions then potions() end
 	end
 end
 
@@ -129,26 +129,26 @@ function ign()
 	if myHero.dead == 0 then
 		igntarget = GetWeakEnemy('TRUE',600)
 		if igntarget ~= nil then
-			IgniteTarget(igntarget, ignKey, , ignopt.BurnGA, ignopt.BurnEgg, ignopt.BurnAatrox, ignopt.BurnZac)
+			IgniteTarget(igntarget, ignKey, ignopt.BurnGA, ignopt.BurnEgg, ignopt.BurnAatrox, ignopt.BurnZac)
 		end
 	end
 end
 
 function potions()
 	if bluePill == nil then
-		if myHero.health < myHero.maxHealth * (CfgPotions.Health_Potion_Value / 100) and GetClock() > wUsedAt + 15000 then
+		if myHero.health < myHero.maxHealth * (Potions.Health_Potion_Value / 100) and GetClock() > wUsedAt + 15000 then
 			usePotion()
 			useBiscuit()
 			wUsedAt = GetTick()
-		elseif myHero.health < myHero.maxHealth * (CfgPotions.Crystalline_Flask_Value / 100) and GetClock() > vUsedAt + 10000 then 
+		elseif myHero.health < myHero.maxHealth * (Potions.Crystalline_Flask_Value / 100) and GetClock() > vUsedAt + 10000 then 
 			useFlask()
 			vUsedAt = GetTick()
-		elseif myHero.health < myHero.maxHealth * (CfgPotions.Biscuit_Value / 100) then
+		elseif myHero.health < myHero.maxHealth * (Potions.Biscuit_Value / 100) then
 			useBiscuit()
-		elseif myHero.health < myHero.maxHealth * (CfgPotions.Elixir_of_Fortitude_Value / 100) then
+		elseif myHero.health < myHero.maxHealth * (Potions.Elixir_of_Fortitude_Value / 100) then
 			useElixir()
 		end
-		if myHero.mana < myHero.maxMana * (CfgPotions.Mana_Potion_Value / 100) and GetClock() > mUsedAt + 15000 then
+		if myHero.mana < myHero.maxMana * (Potions.Mana_Potion_Value / 100) and GetClock() > mUsedAt + 15000 then
 			useManaPot()
 			mUsedAt = GetTick()
 		end
