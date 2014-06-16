6d527f981e8cebd03e5959080920af836ab5cba7
MadCoderz <mail223@linuxmail.org>
2014-06-16 09:12:10 +0300

    clean-up code, Added potions, thanks to unknown developer

--- activator_beta.lua
+++ activator_beta.lua
@@ -4,13 +4,25 @@
 -- Sida for utils.lua
 -- Akira for Activator idea and base (mostly rewritten)
 -- Lua for IgniteTarget.lua
+-- unknown developer for RedElixir.lua (Potions)
 
-require 'Utils'
+require 'utils'
+require 'winapi'
+require 'SKeys'
 local send = require 'SendInputScheduled'
 local uiconfig = require 'uiconfig'
 
 local version = '1.0'
 local target
+local brktarget
+local dfgtarget
+local igntarget
+
+local wUsedAt = 0
+local vUsedAt = 0
+local mUsedAt = 0
+local timer = os.clock()
+local bluePill = nil
 
 ADItems, ad = uiconfig.add_menu('AD Aggressive Items')
 ad.keytoggle('botRK', 'Blade of the Ruined King', true)
@@ -28,8 +40,8 @@ if (myHero.SummonerD or myHero.SummonerF) == SummonerSpells.Ignite.Name then
 	ignopt.checkbutton('BurnEgg', 'Ignite Anivia even Eggnivia passive', false)
 	ignopt.checkbutton('BurnAatrox', 'Ignite Aatrox even Rebirth passive', false)
 	ignopt.checkbutton('BurnZac', 'Ignite Zac even Rebirth passive', false)
-	if myHero.SummonerD == 'SummonerDot' then ignKey = 'D'
-	elseif myHero.SummonerF == 'SummonerDot' then ignKey = 'F'
+	if myHero.SummonerD == SummonerSpells.Ignite.Name then ignKey = 'D'
+	else ignKey = 'F'
 	end
 end
 elseif (myHero.SummonerD or myHero.SummonerF) == SummonerSpells.Exhaust.Name then
@@ -47,13 +59,19 @@ elseif (myHero.SummonerD or myHero.SummonerF) == SummonerSpells.Clairvoyance.Nam
 else
 	print('Script is broken or outdated.')
 end
-
-Pots, pot = uiconfig.add_menu('Pots')
-pot.keytoggle('HP', 'Health Pot', true)
-pot.slider('HPeh', 'HP%', 20, 30, 40) 
-pot.keytoggle('MANA', 'Mana Pot', true)
-pot.slider('MPeh', 'MP%', 20, 30, 40) 
-
+Potions, pots = uiconfig.add_menu('Potions', 200)
+pots.checkbutton('AutoPotions', 'Master Switch: Potions', true)
+pots.checkbutton('Health_Potion_ONOFF', 'Health Potions', true)
+pots.checkbutton('Mana_Potion_ONOFF', 'Mana Potions', true)
+pots.checkbutton('Crystalline_Flask_ONOFF', 'Crystalline Flask', true)
+pots.checkbutton('Elixir_of_Fortitude_ONOFF', 'Elixir of Fortitude', true)
+pots.checkbutton('Biscuit_ONOFF', 'Biscuit', true)
+pots.slider('Health_Potion_Value', 'Health Potion Value', 0, 100, 75, nil, true)
+pots.slider('Mana_Potion_Value', 'Mana Potion Value', 0, 100, 75, nil, true)
+pots.slider('Crystalline_Flask_Value', 'Crystalline Flask Value', 0, 100, 75, nil, true)
+pots.slider('Elixir_of_Fortitude_Value', 'Elixir of Fortitude Value', 0, 100, 30, nil, true)
+pots.slider('Biscuit_Value', 'Biscuit Value', 0, 100, 60, nil, true)
+pots.permashow('AutoPotions')
 local SummonerSpells =
 	{
 		Ignite = {Key = nil, Name = "SummonerDot"},
@@ -65,59 +83,44 @@ local SummonerSpells =
 		Cleanse = {Key = nil, Name = "SummonerBoost"}
 	}
 
-function main()
-	if Pots.HP then HPPot() end
-	if Pots.MANA then MPPot() end
-end
-
-function botRK()
-if myHero.dead == 0 then
-	brk = getInventorySlot(3153)
-	if brk ~= nil and myHero["SpellTime"..brk] >= 1 then
-		brkenemy = GetWeakEnemy('PHYS', 450)
-		if ad.botRK and brkenemy ~= nil and (myHero.maxHealth-myHero.health) <= (brkenemy.maxHealth*15/100) then CastSpellTarget(tostring(brk),brkenemy) end
+function eventLoop()
+	if myHero.dead == 0 then
+		if ad.botRK then botRK() end
+		if ad.Tiamat then Tiamat() end
+		if ad.Hydra then Hydra() end
+		if ap.DFG then DFG() end
+		if Spells.ign then ign() end
+		if Potions.AutoPotions then potions() end
 	end
 end
 
-function Tiamat()
-	if myHero.dead == 0 then
-		tia = GetInventorySlot(3077)
-		if tia ~= nil and myHero["SpellTime"..tia] >= 1 then
-			if ad.Tiamat and GetWeakEnemy('PHYS', 400) ~= nil then CastSpellTarget(tostring(tia),myHero) end
+function botRK()
+	brk = getInventorySlot(3153)
+		if brk ~= nil and myHero["SpellTime"..brk] >= 1 then
+			brktarget = GetWeakEnemy('PHYS', 450)
+			if ad.botRK and brktarget ~= nil and (myHero.maxHealth-myHero.health) <= (brktarget.maxHealth*15/100) then CastSpellTarget(tostring(brk),brktarget) end
 		end
+end
+
+function Tiamat()
+	tia = GetInventorySlot(3077)
+	if tia ~= nil and myHero["SpellTime"..tia] >= 1 then
+		if ad.Tiamat and GetWeakEnemy('PHYS', 400) ~= nil then CastSpellTarget(tostring(tia),myHero) end
 	end
 end
 
 function Hydra()
-	if myHero.dead == 0 then
-		hyd = GetInventorySlot(3074)
-		if hyd ~= nil and myHero["SpellTime"..tia] >= 1 then
-			if ad.Hydra and GetWeakEnemy('PHYS', 400) ~= nil then CastSpellTarget(tostring(hyd),myHero) end
-		end
+	hyd = GetInventorySlot(3074)
+	if hyd ~= nil and myHero["SpellTime"..tia] >= 1 then
+		if ad.Hydra and GetWeakEnemy('PHYS', 400) ~= nil then CastSpellTarget(tostring(hyd),myHero) end
 	end
 end
 
 function DFG()
-	if myHero.dead == 0 then
-		dfg = GetInventorySlot(3074)
-		if dfg ~= nil and myHero["SpellTime"..dfg] >= 1 then
-			dfgenemy = GetWeakEnemy('PHYS', 750)
-			if ap.DFG and dfgenemy ~= nil then CastSpellTarget(tostring(dfg),dfgenemy) end
-		end
-	end
-end
-
-function HPPot()
-		if myHero.health < ((myHero.maxHealth*pot.HPeh)/100) then
-			GetInventorySlot(2003)
-			UseItemOnTarget(2003, myHero)
-		end
-end
-
-function MPPot()
-	if myHero.mana < ((myHero.maxMana*pot.MPeh)/100) then
-		GetInventorySlot(2004)
-		UseItemOnTarget(2004, myHero)
+	dfg = GetInventorySlot(3074)
+	if dfg ~= nil and myHero["SpellTime"..dfg] >= 1 then
+		dfgtarget = GetWeakEnemy('PHYS', 750)
+		if ap.DFG and dfgtarget ~= nil then CastSpellTarget(tostring(dfg),dfgtarget) end
 	end
 end
 
@@ -129,4 +132,63 @@ function ign()
 		end
 	end
 end
-SetTimerCallback("main")
+
+function potions()
+	if bluePill == nil then
+		if myHero.health < myHero.maxHealth * (CfgPotions.Health_Potion_Value / 100) and GetClock() > wUsedAt + 15000 then
+			usePotion()
+			useBiscuit()
+			wUsedAt = GetTick()
+		elseif myHero.health < myHero.maxHealth * (CfgPotions.Crystalline_Flask_Value / 100) and GetClock() > vUsedAt + 10000 then 
+			useFlask()
+			vUsedAt = GetTick()
+		elseif myHero.health < myHero.maxHealth * (CfgPotions.Biscuit_Value / 100) then
+			useBiscuit()
+		elseif myHero.health < myHero.maxHealth * (CfgPotions.Elixir_of_Fortitude_Value / 100) then
+			useElixir()
+		end
+		if myHero.mana < myHero.maxMana * (CfgPotions.Mana_Potion_Value / 100) and GetClock() > mUsedAt + 15000 then
+			useManaPot()
+			mUsedAt = GetTick()
+		end
+	end
+	if (os.clock() < timer + 5000) then
+		bluePill = nil 
+	end
+end
+function OnCreateObj(object)
+	if (GetDistance(myHero, object)) < 100 then
+		if string.find(object.charName,"FountainHeal") then
+			timer=os.clock()
+			bluePill = object
+		end
+	end
+end
+function usePotion()
+	GetInventorySlot(2003)
+	UseItemOnTarget(2003,myHero)
+end
+function useBiscuit()
+	GetInventorySlot(2009)
+	UseItemOnTarget(2009,myHero)
+end
+function useFlask()
+	GetInventorySlot(2041)
+	UseItemOnTarget(2041,myHero)
+end
+function useBiscuit()
+	GetInventorySlot(2009)
+	UseItemOnTarget(2009,myHero)
+end
+function useElixir()
+	GetInventorySlot(2037)
+	UseItemOnTarget(2037,myHero)
+end
+function useManaPot()
+	GetInventorySlot(2004)
+	UseItemOnTarget(2004,myHero)
+end
+function GetTick()
+	return GetClock()
+end
+SetTimerCallback("eventLoop")
